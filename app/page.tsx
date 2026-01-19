9c6f76423cea27618fe950d6253be610c00c6412
kingsedu <chinedumeh26@yahoo.com>
2026-01-19 22:37:26 +0100

    add about

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Stack from "@/components/Stack";
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <Hero />
       <Stack />
+      <About />
     </>
   );
 }
